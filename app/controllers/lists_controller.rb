8e3a38f2c3aedd8f745d9b5442a9affc21dea90b
Wendy Pham <wendypham_s2@hotmail.com>
2022-04-19 22:05:48 -0400

    completed methods in lists controller and added views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,4 +6,24 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
