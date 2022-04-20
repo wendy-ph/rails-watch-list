eedb2dd60cf97f924ef61bd6ff09ca526ff47cf7
Wendy Pham <wendypham_s2@hotmail.com>
2022-04-19 23:14:16 -0400

    created bookmarks controller, methods and views

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,32 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    # @bookmarks = find_bookmark
   end
 
   def new
@@ -20,6 +21,15 @@ class ListsController < ApplicationController
     end
   end
 
+  # def find_bookmark
+  #   @bookmarks = []
+  #   # @bookmarks = Bookmark.find(@list.bookmark_ids[0])
+  #   @list.bookmark_ids.each do |id|
+  #     bookmark = Bookmark.find(id)
+  #     @bookmarks << bookmark
+  #   end
+  #   @bookmarks
+  # end
 
   private
 
