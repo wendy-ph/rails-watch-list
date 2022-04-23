e5eee22c487f9edb720a068c41c54d2e22416c98
Wendy Pham <wendypham_s2@hotmail.com>
2022-04-23 11:05:19 -0400

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :lists, only: [ :index, :show, :new, :create ] do
+  root to: 'lists#index'
+  resources :lists, only: [ :show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create ]
   end
 
